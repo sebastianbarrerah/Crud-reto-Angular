7106dc6e67f0561ef90e70a66087dd60831a3b6e
Sebastian Barrera Herrera <sebastian.barrerah@sofka.local>
2024-07-08 22:55:28 -0500

    comentando el guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './users/pages/login-page/login-page.component';
 import { RegisterPageComponent } from './users/pages/register-page/register-page.component';
 import { HomeComponent } from './notes/pages/home/home.component';
-import { authGuard } from './users/guards/auth.guard';
+// import { authGuard } from './users/guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -16,7 +16,7 @@ export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent,
-        canActivate: [authGuard]
+        // canActivate: [authGuard]
     },
     {
         path: '**',
--- src/app/users/guards/login.guard.ts
+++ src/app/users/guards/login.guard.ts
@@ -1,12 +1,12 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+// import { inject } from '@angular/core';
+// import { CanActivateFn, Router } from '@angular/router';
 
-export const loginGuard: CanActivateFn = (route, state) => {
-  const routes = inject(Router)
-  if (localStorage.getItem("state") == "true") {
-    return false
-  }else{
-    routes.navigate(["home"])
-    return true;
-  }
-};
+// export const loginGuard: CanActivateFn = (route, state) => {
+//   const routes = inject(Router)
+//   if (localStorage.getItem("state") == "true") {
+//     return false
+//   }else{
+//     routes.navigate(["home"])
+//     return true;
+//   }
+// };
