636b10f0120c4545dfff32239f0cd2078b173ba8
Sebastian Barrera Herrera <sebastian.barrerah@sofka.local>
2024-07-08 23:06:27 -0500

    add config rutas

--- src/app/users/guards/auth.guard.ts
+++ src/app/users/guards/auth.guard.ts
@@ -1,13 +1,13 @@
-// import { inject } from '@angular/core';
-// import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
-// export const authGuard: CanActivateFn = (route, state) => {
-//   const routes = inject(Router)
-//   if (localStorage.getItem("state") == "true") {
-//     return true
-//   }else{
-//     routes.navigate(["login"])
-//     return false;
-//   }
-// };
+export const authGuard: CanActivateFn = (route, state) => {
+  const routes = inject(Router)
+  if (localStorage.getItem("state") == "true") {
+    return true
+  }else{
+    routes.navigate(["login"])
+    return false;
+  }
+};
 
